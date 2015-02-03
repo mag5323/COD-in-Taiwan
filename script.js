fc7dd6ecea9948098ad224f843ce39589c5ceb8f
mag5323 <mag5323@gmail.com>
2015-02-03 17:44:07 +0800

    render 80year init

--- script.js
+++ script.js
@@ -58,4 +58,5 @@ window.onload = function() {
     options.push('<option value=' + years[key] + '>民國 ' + years[key] + ' 年</option>');
   }
   document.getElementById('pie1-select').innerHTML = options.join('');
+  renderPie(80, 'pie1');
 }
