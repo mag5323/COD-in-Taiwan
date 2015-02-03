b37ad0be8ac56c46867c31aa2caf4cf975721828
mag5323 <mag5323@gmail.com>
2015-02-03 17:37:08 +0800

    refactoring script

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-window.onload = function() {
-  d3.csv('data/dead102.txt')
+d3.csv('data/dead102.txt')
+.get(function(error, data) {
+  var mailiao = _.filter(data, { 'county': '3913' });
+  var cause = _.groupBy(mailiao, 'cause');
+
+  d3.csv('data/after97cause.txt')
     .get(function(error, data) {
-      var mailiao = _.filter(data, { 'county': '3913' });
-      var cause = _.groupBy(mailiao, 'cause');
+      var mapping = data;
+      var data = {
+        sortOrder: "value-asc",
+        content: []
+      };
 
-      d3.csv('data/after97cause.txt')
-        .get(function(error, data) {
-          var mapping = data;
-          var data = {
-            sortOrder: "value-asc",
-            content: []
-          };
+      for (key in cause) {
+        data.content.push({label: mapping[key - 1].cause, value: cause[key].length});
+      }
 
-          for (key in cause) {
-            data.content.push({label: mapping[key - 1].cause, value: cause[key].length});
+      var output = {
+        header: {
+          title: {
+            text: '雲林縣麥寮鄉102年死因統計'
           }
+        },
+        labels: {
+          inner: {
+            format: 'percentage'
+          }
+        },
+        tooltips: {
+          enabled: true,
+          type: 'placeholder',
+          string: '{value} 人'
+        },
+        size: {
+          'canvasHeight': 700,
+          'canvasWidth': 900
+        },
+        data: data
+      };
 
-          var output = {
-            header: {
-              title: {
-                text: '雲林縣麥寮鄉102年死因統計'
-              }
-            },
-            labels: {
-              inner: {
-                format: 'percentage'
-              }
-            },
-            tooltips: {
-              enabled: true,
-              type: 'placeholder',
-              string: '{value} 人'
-            },
-            size: {
-              'canvasHeight': 700,
-              'canvasWidth': 900
-            },
-            data: data
-          };
-
-          var pie = new d3pie('pie1', output);
-        });
+      var pie = new d3pie('pie1', output);
     });
+});
 
+function changeFile(e) {
+  var year = e.value;
+  var dead  = 'dead' + year + '.txt';
+  var mapping = year > 96 ? 'after97cause.txt' : 'before97cause.txt';
+}
+
+window.onload = function() {
   var years = _.range(80, 103);
   var options = [];
   for (key in years) {
     options.push('<option value=' + years[key] + '>民國 ' + years[key] + ' 年</option>');
   }
   document.getElementById('pie1-select').innerHTML = options.join('');
-}
-
-function changeFile(e) {
-  var year = e.value;
-  var dead  = 'dead' + year + '.txt';
-  var mapping = year > 96 ? 'after97cause.txt' : 'before97cause.txt';
 }
